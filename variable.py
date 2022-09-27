8b610cd876aaed28b5f91d2af9a2f1654bb9236c
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-09-27 15:07:55 +0100

    playing with variables

---
+++ variable.py
@@ -0,0 +1,9 @@
+from distutils.command.build_scripts import first_line_re
+
+
+first_name = "John"
+last_name = "Smith"
+age = 20
+patient_status = "new patient"
+
+print(f"Patient name: {first_name} {last_name}\nAge: {age}\nPatient status: {patient_status}")
