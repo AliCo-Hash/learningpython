96b764c7d6bcfaf8b9bc831bab3a3c10a75c7a0c
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-09-27 16:39:59 +0100

    playing with datetime

--- birthdaychecker/birthday.py
+++ birthdaychecker/birthday.py
@@ -1,3 +1,8 @@
-birth_year = input("What is your birth year? ")
-age = 2022 - int(birth_year)
-print(f"You are {age} years old")
+import datetime
+
+#birthday = input("When is your birthday in DD/MM/YYYY? ")
+current_date = datetime.date.today()
+today = current_date.strftime("%d/%m/%Y")
+print(today)
+#age = today - int(birthday)
+#print(f"You are {age} years old")
