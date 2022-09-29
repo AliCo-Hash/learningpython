82d568526b4bc014c983590f0526341cf03b5418
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-09-29 16:06:09 +0100

    testing addition of floats

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -6,7 +6,7 @@ class TestCalc(unittest.TestCase):
   def test_add(self):
     self.assertEqual(Calculator.add(10, 20), 30)
     self.assertEqual(Calculator.add(5, 4), 9)
-
+    self.assertEqual(Calculator.add(2.3, 4.2), 6.5)
 
 
 if __name__ == '__main__':
