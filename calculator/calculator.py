bc79487713d181e67244a51f27cf1a5403e6e13e
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-09-29 17:28:59 +0100

    powerof now works for negative powers

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -13,8 +13,16 @@ class Calculator:
     return x / y
 
   def powerof(x, y):
-    count = 1
-    while y > 0:
-      count = count * x
-      y -= 1
-    return count
+    if y > 0:
+      count = 1
+      while y > 0:
+        count = count * x
+        y -= 1
+      return count
+    elif y < 0:
+      y = -y
+      count = 1
+      while y > 0:
+        count = count * 1/x
+        y-=1
+      return count
